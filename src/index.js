94000ccbaca0206c324f26f3561973fdb342e331
pokeyuke <augustogc134@gmail.com>
2025-10-30 22:03:06 +0100

    Created the principal js page
    Still working on the logic part. My idea was to create a separated class for the todos and another for a project, that can contain one or more todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import "./styles.css";
+
+
+class ToDo{
+
+    constructor(title,description,dueDate,priority, notes, completed){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.completed = completed //bool
+
+        }
+}
+
+class Project{
+    static quantity = 0;
+
+
+    constructor(name){
+        this.name = name;
+        this.todos = [];  // lista vuota di ToDos
+        Project.quantity++; // quanti progetti ci sono 
+    }
+    
+
+    addToDo(titles,description,dueDate,priority,notes,completed){
+        const newToDo = new ToDo(titles,description,dueDate,priority,notes,completed)    
+        this.todos.push(newToDo);
+    }
+
+    removeToDo(index){
+        this.todos.splice(index, 1) // rimuove il toDo a partire dalla sua posizione
+    }
+
+    filterByDate(){
+
+    }
+
+    filterByPriority(){
+
+    }
+
+}
+
+
+
+
+
+const project = new project();
