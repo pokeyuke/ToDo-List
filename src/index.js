38f0bf4332d907b5e2f489d666f0212a1e024f1e
pokeyuke <augustogc134@gmail.com>
2025-11-02 13:01:33 +0100

    Improved the logic part
    Created the ProjectManager class, with set/Get and add/remove projects

--- src/index.js
+++ src/index.js
@@ -5,56 +5,91 @@ console.log("HIIIIIII")
 
 
 
-class ToDo{
+    class ToDo{
 
-    constructor(title,description,dueDate,priority, notes, completed){
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;  //   0 <= i <= 10
-        this.notes = notes;
-        this.completed = completed //bool
+        constructor(title,description,dueDate,priority, notes, completed){
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;  //   0 <= i <= 10
+            this.notes = notes;
+            this.completed = completed //bool
 
+            }
+
+            toggleComplete(completed){
+            this.completed = true
+            }
+
+    }
+
+    class Project{
+         // quanti progetti ci sono inizialmente
+        static idCounter = 0;
+
+        constructor(name){
+            this.name = name;
+            this.todos = [];  // lista vuota di ToDos
+            this.id = Project.idCounter++
         }
 
-        toggleComplete(completed){
-        this.completed = true
+    
+
+        addToDo(titles,description,dueDate,priority,notes,completed){
+            const newToDo = new ToDo(titles,description,dueDate,priority,notes,completed)    
+            this.todos.push(newToDo);  // quanti t
         }
 
-}
+        removeToDo(index){
+            this.todos.splice(index, 1) // rimuove il toDo a partire dalla sua posizione
+        }
+    
+            
+            sortByMaxPriority(){    //sort the array by decresing priority
+                return this.todos.toSorted((a,b) => b.priority - a.priority);
+                    
+            }
 
-class Project{
-    static quantity = 0; // quanti progetti ci sono inizialmente
+            sortByMinPriority(){
+                return this.todos.toSorted((a,b) => b.priority - a.priority)
+            }
 
-
-    constructor(name){
-        this.name = name;
-        this.todos = [];  // lista vuota di ToDos
-        Project.quantity++; // quanti progetti ci sono 
     }
 
-   
+    class ProjectManager{
 
-    addToDo(titles,description,dueDate,priority,notes,completed){
-        const newToDo = new ToDo(titles,description,dueDate,priority,notes,completed)    
-        this.todos.push(newToDo);  // quanti t
-    }
-
-    removeToDo(index){
-        this.todos.splice(index, 1) // rimuove il toDo a partire dalla sua posizione
-    }
-  
         
-        sortByMaxPriority(){    //sort the array by decresing priority
-            return this.todos.toSorted((a,b) => b.priority - a.priority);
-                
+
+        constructor(){
+            this.projects = [];
         }
 
-        sortByMinPriority(){
-            return this.todos.toSorted((a,b) => b.priority - a.priority)
+        addProject(name){
+            const newProject = new Project(name);
+            this.projects.push(newProject)
         }
 
-}
+        removeProject(index){
+            this.projects.splice(index, 1);
+        }
+
+        getCurrentProject(){
+            return
+        }
+
+        setCurrentProject(id){
+
+        }
+
+
+
+    }
+
+        // ONLY ONE PROJECT MANAGER: SINGLETON PATTERN
+        const manager = new ProjectManager()
+        manager.addProject("Default");
+
+        export default manager
     
 
 
